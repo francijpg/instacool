b74220e6a56c73edbd1890ce30eb391c6d372af5
francijpg <hellofranciscopg@gmail.com>
2020-08-13 13:37:14 -0400

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, CSSProperties } from "react";
 
 const style = {
   backgroundColor: "#fff",
   border: "1px solid #ddd",
   padding: "10px 15px",
   borderRadius: "4px",
-  width: 'calc(100% - 30px)',//for to stack content, (input width - left/right(15px) padding)
-  marginBottom: '10px',
+  width: "calc(100% - 30px)", //for to stack content, (input width - left/right(15px) padding)
+  marginBottom: "10px",
 };
+
+const spanStyle ={
+  color: "#777",
+  fontSize: '10px',
+  textTransform: 'uppercase',
+  fontWeight: 900,
+} as CSSProperties
 
 interface IInputProps {
   placeholder?: string;
@@ -15,6 +22,11 @@ interface IInputProps {
 
 export default class Input extends Component<IInputProps> {
   render() {
-    return <input {...this.props} style={style} />;
+    return (
+      <div>
+        <span style={spanStyle}>text</span>
+        <input {...this.props} style={style} />
+      </div>
+    );
   }
 }
