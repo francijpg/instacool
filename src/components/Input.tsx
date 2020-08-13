924e105bf85a44334774d8cb8207d8cbc0179ba5
francijpg <hellofranciscopg@gmail.com>
2020-08-13 13:28:59 -0400

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,6 +5,8 @@ const style = {
   border: "1px solid #ddd",
   padding: "10px 15px",
   borderRadius: "4px",
+  width: 'calc(100% - 30px)',//for to stack content, (input width - left/right(15px) padding)
+  marginBottom: '10px',
 };
 
 interface IInputProps {
