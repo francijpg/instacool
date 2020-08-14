56cb035a62edaeb822b054ceccbd7ac827d4d36e
francijpg <hellofranciscopg@gmail.com>
2020-08-14 13:32:31 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import Login from "./containers/Auth/Login";
 import Register from "./containers/Auth/Register";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Route exact={true} path="/" component={Login} />
       <Route exact={true} path="/register" component={Register} />
-    </div>
+    </BrowserRouter>
   );
 }
 
