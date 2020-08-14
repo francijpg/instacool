721bc383dfb3bfd1fc66cedaf8295b2c3121eb8c
francijpg <hellofranciscopg@gmail.com>
2020-08-14 17:31:44 -0400

    Update index.tsx

--- src/containers/NewsFeed/index.tsx
+++ src/containers/NewsFeed/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Post from "../../components/Post";
 import Container from "../../components/Container";
 
@@ -6,8 +6,10 @@ export default class NewsFeed extends Component {
   render() {
     return (
       <Container>
-        <div><Post /></div>
+        <div style={{ margin: "0 auto" }}>
+          <Post image={"http://placekitten.com/300/200"}/>
+        </div>
       </Container>
-    )
+    );
   }
 }
