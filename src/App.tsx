4176dea817afc47341e1a0ff3c9276e1463d43e3
francijpg <hellofranciscopg@gmail.com>
2020-08-14 12:02:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import "./App.css";
 import Card from "./components/Card";
 import Container from "./components/Container";
 import Input from "./components/Input";
+import Button from "./components/Button";
+import Title from "./components/Title";
+import Link from "./components/Link";
+import Center from "./components/Center";
 
 function App() {
   return (
     <Container>
       <Card>
-        <Input placeholder="correo" label="Correo"/>
-        <Input placeholder="contraseña" label="Contraseña"/>
+        <Title>Iniciar sesión</Title>
+        <Input placeholder="correo" label="Correo" />
+        <Input placeholder="contraseña" label="Contraseña" />
+        <Button block={true}>Enviar</Button>
+        <Center>
+          <Link>Ir al registro</Link>
+        </Center>
       </Card>
     </Container>
   );
