763ead75de5aee5f1ffa80414ba5b472a9edf310
francijpg <hellofranciscopg@gmail.com>
2020-08-15 10:07:45 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Login from "./containers/Auth/Login";
 import Register from "./containers/Auth/Register";
 import NewsFeed from "./containers/NewsFeed";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact={true} path="/" component={Login} />
       <Route exact={true} path="/register" component={Register} />
+      <Route path="/app" component={Navbar} />
       <Route exact={true} path="/app/newsfeed" component={NewsFeed} />
     </BrowserRouter>
   );
