bcc2e0e8ba72321a949f0cf0900115c5211d6cbb
francijpg <hellofranciscopg@gmail.com>
2020-08-15 09:40:28 -0400

    Update Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faRetweet } from '@fortawesome/free-solid-svg-icons'
 
 const style = {
   backgroundColor: "#fff",
@@ -8,15 +10,27 @@ const style = {
 };
 
 interface IPostProps {
-  image: string
+  image: string;
 }
 
 export default class Post extends Component<IPostProps> {
   render() {
-    const { image } = this.props
+    const { image } = this.props;
     return (
       <div style={style}>
         <img src={image} />
+        <div
+          style={{
+            display: "flex",
+            backgroundColor: "#eee",
+            marginLeft: "-15px",
+            marginBottom: "-10px",
+            width: "calc(100% + 30px)",
+          }}
+        >
+          <div style={{ flex: 1, textAlign: "center", padding:"10px 15px", cursor: "pointer" }}><FontAwesomeIcon icon={faThumbsUp}/> Like</div>
+          <div style={{ flex: 1, textAlign: "center", padding:"10px 15px", cursor: "pointer" }}><FontAwesomeIcon icon={faRetweet}/> Compartir</div>
+        </div>
       </div>
     );
   }
