d3dbcea3d45db51a3f935056fcd13578a83b8ce0
francijpg <hellofranciscopg@gmail.com>
2020-08-15 10:07:48 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+const style = {
+  borderBottom: "solid 1px #aaa",
+  padding: "10px 15px",
+};
 
 export default class Navbar extends Component {
   render() {
-    return (
-      "Navbar"
-    )
+    return <div style={style}>Navbar</div>;
   }
 }
