a4715bff770dacfb44b72a9c4bc05c49112071b4
francijpg <hellofranciscopg@gmail.com>
2020-08-16 00:06:55 -0400

    Create Profileimg.tsx

---
+++ src/components/Profileimg.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+const style = {
+  img: {
+    borderRadius: "100%",
+  },
+};
+
+export default class Profileimg extends Component {
+  render() {
+    return (
+      <div>
+        <img style={style.img} src="http://placekitten.com/100/100" alt="profile"/>
+      </div>
+    )
+  }
+}
