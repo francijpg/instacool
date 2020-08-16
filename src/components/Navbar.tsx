9f04016ff1a72b3b140c1f24cda879b39d310a9d
francijpg <hellofranciscopg@gmail.com>
2020-08-16 00:08:51 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNewspaper, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
 const style = {
-  borderBottom: "solid 1px #aaa",
-  padding: "10px 15px",
+  navbar: {
+    borderBottom: "solid 1px #aaa",
+    padding: "10px 15px",
+  },
+  link: {
+    color: "#555",
+    textDecoration: "none",
+  },
 };
 
 export default class Navbar extends Component {
   render() {
-    return <div style={style}>Navbar</div>;
+    return (
+      <div style={style.navbar}>
+        <Link style={style.link} to="/app/newsfeed"><FontAwesomeIcon icon={faNewspaper}/> Instacool</Link>
+        <div style={{ float: "right"}}><Link style={style.link} to="/app/profile"><FontAwesomeIcon icon={faUser}/> Perfil</Link></div>
+      </div>
+    );
   }
 }
