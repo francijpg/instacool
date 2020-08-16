74376cd766338139e9f0c088c6a58c06a5e2b8b8
francijpg <hellofranciscopg@gmail.com>
2020-08-16 07:29:13 -0400

    Update index.tsx

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import ProfileImg from "../../components/ProfileImg";
+import Button from "../../components/Button";
 
 const style = {
   container: {
     padding: "15px",
+    display: "flex",
+    justifyContent: "space-between", //add 100% spaces beetween all element contend (profile and button), good for work with columns
   },
 };
 export default class Profile extends Component {
@@ -11,6 +14,7 @@ export default class Profile extends Component {
     return (
       <div style={style.container}>
         <ProfileImg />
+        <Button>Agregar</Button>
       </div>
     );
   }
