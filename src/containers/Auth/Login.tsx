257bfcfe2625d0efacdac2201772f881579d029e
francijpg <hellofranciscopg@gmail.com>
2020-08-17 09:34:26 -0400

    implement LoginForm into Login component

--- src/containers/Auth/Login.tsx
+++ src/containers/Auth/Login.tsx
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 import Card from "../../components/Card";
 import Container from "../../components/Container";
-import Input from "../../components/Input";
-import Button from "../../components/Button";
 import Title from "../../components/Title";
-import Center from "../../components/Center";
+import LoginForm from "../../components/LoginForm";
 
 export default class Login extends Component {
   render() {
@@ -14,12 +11,7 @@ export default class Login extends Component {
       <Container center={true}>
         <Card>
           <Title>Iniciar sesión</Title>
-          <Input placeholder="correo" label="Correo" />
-          <Input placeholder="contraseña" label="Contraseña" />
-          <Button block={true}>Enviar</Button>
-          <Center>
-            <Link to="/register">Ir al registro</Link>
-          </Center>
+          <LoginForm />
         </Card>
       </Container>
     );
