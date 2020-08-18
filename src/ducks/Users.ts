91ef22261dcd9caeab0b408d1d08e537b236ce08
francijpg <hellofranciscopg@gmail.com>
2020-08-18 12:08:37 -0400

    create users collection from register thunk

--- src/ducks/Users.ts
+++ src/ducks/Users.ts
@@ -15,7 +15,10 @@ export const login = ({ email, password }: ILogin) =>
     await auth.signInWithEmailAndPassword(email, password)
 
 export const register = ({ email, password }: ILogin) =>
-  async (dispatch: Dispatch, getState: () => any, { auth }: IServices) => {
-    const user = await auth.createUserWithEmailAndPassword(email, password)
-    console.log(user)
+  async (dispatch: Dispatch, getState: () => any, { auth, db }: IServices) => {
+    const userCredential = await auth.createUserWithEmailAndPassword(email, password)
+    const { user } = userCredential
+    const id = user ? user.uid : undefined
+    const doc = db.collection('users').doc(id)
+    await doc.set({ role: 'user' })
   }
