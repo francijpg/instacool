a04f1576ad751eff64ddd8886c78a766b0a3a946
francijpg <hellofranciscopg@gmail.com>
2020-08-18 09:14:30 -0400

    add auth.onAuthStateChanged + simple loading

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,37 @@ import Register from "./containers/Auth/Register";
 import NewsFeed from "./containers/NewsFeed";
 import Navbar from "./components/Navbar";
 import Profile from "./containers/Profile";
+import services from "./services";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Route exact={true} path="/" component={Login} />
-      <Route exact={true} path="/register" component={Register} />
-      <Route path="/app" component={Navbar} />
-      <Route exact={true} path="/app/newsfeed" component={NewsFeed} />
-      <Route exact={true} path="/app/profile" component={Profile} />
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    const { auth } = services;
+    auth.onAuthStateChanged((user) => {
+      console.log(user);
+      this.setState({
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    const { loading } = this.state;
+    return loading ? (
+      "loading"
+    ) : (
+      <BrowserRouter>
+        <Route exact={true} path="/" component={Login} />
+        <Route exact={true} path="/register" component={Register} />
+        <Route path="/app" component={Navbar} />
+        <Route exact={true} path="/app/newsfeed" component={NewsFeed} />
+        <Route exact={true} path="/app/profile" component={Profile} />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
