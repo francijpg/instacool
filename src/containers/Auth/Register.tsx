96df5c2ec1d64b1b09f6685d5d9065b1230ddd45
francijpg <hellofranciscopg@gmail.com>
2020-08-18 10:16:38 -0400

    Update Register.tsx

--- src/containers/Auth/Register.tsx
+++ src/containers/Auth/Register.tsx
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-
 import Card from "../../components/Card";
 import Container from "../../components/Container";
-// import Input from "../../components/Input";
-import Button from "../../components/Button";
 import Title from "../../components/Title";
-import Center from "../../components/Center";
+import RegisterForm from "../../components/RegisterForm";
 
 export default class Register extends Component {
   render() {
@@ -14,12 +10,7 @@ export default class Register extends Component {
       <Container center={true}>
         <Card>
           <Title>Registro</Title>
-          {/* <Input placeholder="correo" label="Correo" />
-          <Input placeholder="contraseña" label="Contraseña" /> */}
-          <Button block={true}>Enviar</Button>
-          <Center>
-            <Link to="/">Iniciar sesión</Link>
-          </Center>
+          <RegisterForm />
         </Card>
       </Container>
     );
