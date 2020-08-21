91a46d57654c739a6c32c29078e9eac1faa03c2d
francijpg <hellofranciscopg@gmail.com>
2020-08-21 11:57:49 -0400

    app detect credentials (key.json)

--- functions/src/createServer.ts
+++ functions/src/createServer.ts
@@ -1,6 +1,10 @@
 import * as express from 'express'
 import * as admin from 'firebase-admin'
 
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+})
+
 const db = admin.firestore()
 const auth = admin.auth()
 
