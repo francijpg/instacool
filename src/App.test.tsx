c6ec1cbd7d7132c4228907858d21504192207ef2
francijpg <hellofranciscopg@gmail.com>
2020-08-25 15:09:11 -0400

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 import { createBrowserHistory } from 'history';
+import { identity } from "lodash";
 
 const history = createBrowserHistory();
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App history={history}/>);
+  const { getByText } = render(<App loadInitialData={ identity } history={history}/>);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
