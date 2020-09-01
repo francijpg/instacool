1ef9d269fe870e9a6a615e382c5e8d83a47b136e
francijpg <hellofranciscopg@gmail.com>
2020-09-01 10:51:08 -0400

    add disabled props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 
-const style = (block: boolean) => ({
-  backgroundColor: "#00D1B2",
+const style = (block: boolean, disabled: boolean) => ({
+  backgroundColor: disabled ? "#777777" : "#00D1B2",
   border: "0px",
   borderRadius: "4px",
   padding: "10px 15px",
   color: "#fff",
   width: block ? "100%" : undefined,
   marginBottom: "10px",
-})
+});
 
 interface IButtonProps {
   block?: boolean;
+  disabled?: boolean;
 }
+
 export default class Button extends Component<IButtonProps> {
   render() {
-    const { block = false, ...otherProps } = this.props;
-    return <button {...otherProps} style={style(block)} />;
+    const { block = false, disabled = false, ...otherProps } = this.props;
+    return (
+      <button
+        {...otherProps}
+        style={style(block, disabled)}
+        disabled={disabled}
+      />
+    );
   }
 }
